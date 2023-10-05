94b5083d68590602e89287bcb4a13e3385e8b19e
krishna-kenny <krishna.kenny4@gmail.com>
2023-10-05 22:02:26 +0530

    pdf has been embedded for resume page

--- src/components/ResumePage.js
+++ src/components/ResumePage.js
@@ -1,9 +1,16 @@
 const ResumePage = () => {
   return (
-    <div>
-      <h1>this is the Resume page</h1>
-    </div>
-  )
-}
+    <>
+      <iframe
+        src="https://drive.google.com/file/d/1FS-ESyMNngq-Yz0rnqdnqsVDZsNImTnr/preview"
+        width="100%"
+        height="100%"
+        allow="autoplay"
+        title="my resume pdf"
+        style={{margin: "0", border: "0", width: "100%"}}
+      ></iframe>
+    </>
+  );
+};
 
 export default ResumePage;
